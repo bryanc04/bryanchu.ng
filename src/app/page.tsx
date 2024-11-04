011012fea120afc3461d8a435dba829adaecc1a6
bryanc04 <87562897+bryanc04@users.noreply.github.com>
2024-11-04 10:13:48 -0500

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from "~/components/about";
 import { Contact } from "~/components/contact";
 import { Footer } from "~/components/footer";
+import React, { useEffect } from "react";
 import { Hero } from "~/components/hero";
 import { Navbar } from "~/components/navbar";
 import { Projects } from "~/components/projects";
@@ -10,8 +11,25 @@ import Awards from "~/components/awards/awards";
 import AnimatedCursor from "react-animated-cursor";
 
 export default function HomePage() {
+  const PreloadSite = (url: any) => {
+    useEffect(() => {
+      // Create an invisible iframe to load the site in the background
+      const iframe = document.createElement("iframe");
+      iframe.style.display = "none";
+      iframe.src = url;
+      document.body.appendChild(iframe);
+
+      return () => {
+        document.body.removeChild(iframe);
+      };
+    }, [url]);
+
+    return null;
+  };
   return (
     <main className="relative">
+      <PreloadSite url="https://pokemon-bryanc004.web.app" />
+
       <AnimatedCursor
         innerSize={8}
         outerSize={35}
