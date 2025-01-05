b855a707ea4d1739f46a1e032e7e75e281413784
bryanc04 <87562897+bryanc04@users.noreply.github.com>
2025-01-05 16:05:44 -0500

    hi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,12 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { Toaster } from "~/components/ui/toaster";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Bryan Chung's Page",
   description: "Check out Bryan's portfolio!",
-  icons: [{ url: "/swirl.svg", rel: "icon" }],
+  icons: [{ url: "/logo.svg", rel: "icon" }],
 };
 
 export default function RootLayout({
@@ -17,8 +18,10 @@ export default function RootLayout({
     <html lang="en" className={`${GeistSans.variable} dark`}>
       <body>
         {children}
+
         <Toaster />
       </body>
+      <GoogleAnalytics gaId="G-DDZ2W9JMSE" />
     </html>
   );
 }
