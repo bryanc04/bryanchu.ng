3bc7a60b6d27d9624e0cc45c93edb159d1de44b4
bryanc04 <87562897+bryanc04@users.noreply.github.com>
2025-01-05 17:45:01 -0500

    hi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { Toaster } from "~/components/ui/toaster";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Bryan Chung's Page",
@@ -18,7 +19,15 @@ export default function RootLayout({
     <html lang="en" className={`${GeistSans.variable} dark`}>
       <body>
         {children}
-
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ppd8fkqut2");
+          `}
+        </Script>
         <Toaster />
       </body>
       <GoogleAnalytics gaId="G-DDZ2W9JMSE" />
