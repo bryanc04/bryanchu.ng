6cbdc1f4536bbb68bb5d09e9da2515464611bbcb
bryanc04 <87562897+bryanc04@users.noreply.github.com>
2025-10-15 14:55:30 -0400

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import Link from "next/link";
-import Iframe from "react-iframe";
 import { Hero } from "~/components/hero";
 import { Projects } from "~/components/projects";
 import Research from "~/components/research/research";
@@ -10,56 +9,39 @@ import Education from "~/components/education/education";
 import Awards from "~/components/awards/awards";
 import { Footer } from "~/components/footer";
 import AnimatedCursor from "react-animated-cursor";
-import Button from "@mui/joy/Button";
-import Modal from "@mui/joy/Modal";
-import ModalClose from "@mui/joy/ModalClose";
-import Typography from "@mui/joy/Typography";
-import Sheet from "@mui/joy/Sheet";
-import Loader from "./Loader"; // Add this import
-import { Player } from "react-simple-player";
-import { AudioPlayer } from "react-audio-play";
+import Loader from "./Loader";
 
-interface PrerenderWebsiteProps {
-  url: string;
-}
 const sections = ["home", "projects", "experiences", "awards", "research"];
 
 export default function HomePage() {
   const [displayGame, setDisplayGame] = React.useState<boolean>(false);
-  const [displayGameModal, setDisplayGameModal] = React.useState(false);
-  const [showModal, setShowModal] = React.useState(false);
   const [loading, setLoading] = React.useState(true);
   const [activeSection, setActiveSection] = React.useState<string>("hero");
   const navbarRef = React.useRef<HTMLUListElement | null>(null);
   const [isMobile, setIsMobile] = React.useState(false);
 
-  // Add useEffect to check screen size
+  // Check screen size
   React.useEffect(() => {
     const checkMobile = () => {
-      setIsMobile(window.innerWidth < 768); // 768px is a common breakpoint for mobile devices
+      setIsMobile(window.innerWidth < 768);
     };
-
-    // Check initially
     checkMobile();
-
-    // Add event listener for window resize
     window.addEventListener("resize", checkMobile);
-
-    // Cleanup
     return () => window.removeEventListener("resize", checkMobile);
   }, []);
 
+  // Observe active section
   React.useEffect(() => {
     const observerOptions = {
-      root: null, // Use the viewport as the root
-      rootMargin: "-50% 0% -50% 0%", // Ensures 60% of the screen is considered
+      root: null,
+      rootMargin: "-50% 0% -50% 0%",
       threshold: 0,
     };
 
     const observerCallback = (entries: IntersectionObserverEntry[]) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          setActiveSection(entry.target.id); // Update active section
+          setActiveSection(entry.target.id);
         }
       });
     };
@@ -69,69 +51,37 @@ export default function HomePage() {
       observerOptions,
     );
 
-    // Observe all sections
     sections.forEach((sectionId) => {
       const section = document.getElementById(sectionId);
       if (section) observer.observe(section);
     });
 
-    return () => {
-      // Cleanup observer on unmount
-      observer.disconnect();
-    };
+    return () => observer.disconnect();
   }, []);
 
-  React.useEffect(() => {
-    const timer = setTimeout(() => {
-      if (!displayGame) {
-        setShowModal(true);
-      }
-    }, 12000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
+  // Loading state
   React.useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
     }, 4000);
-
     return () => clearTimeout(timer);
   }, []);
 
+  // Adjust navbar scroll position
   React.useEffect(() => {
-    // Adjust navbar position to center the active link
     const navbar = navbarRef.current;
     const activeLink = document.querySelector(`#navbar-item-${activeSection}`);
-
     if (navbar && activeLink) {
       const navbarRect = navbar.getBoundingClientRect();
       const activeLinkRect = activeLink.getBoundingClientRect();
-
-      // Calculate the offset to center the active link
       const offset =
         activeLinkRect.top -
         navbarRect.top -
         navbarRect.height / 2 +
         activeLinkRect.height / 2;
-
-      // Apply the transform to the navbar
       navbar.style.transform = `translateY(${-offset - 10}px)`;
     }
   }, [activeSection]);
-
-  // Handle scroll locking when game is displayed
-  React.useEffect(() => {
-    if (displayGame) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "unset";
-    }
-
-    return () => {
-      document.body.style.overflow = "unset";
-    };
-  }, [displayGame]);
 
   return (
     <main className="relative">
@@ -141,44 +91,8 @@ export default function HomePage() {
         innerScale={1}
         outerScale={2}
         outerAlpha={0}
-        innerStyle={{
-          backgroundColor: "white",
-        }}
+        innerStyle={{ backgroundColor: "white" }}
       />
-
-      <div
-        style={{
-          position: "fixed",
-          bottom: "0",
-          width: "100vw",
-          zIndex: 1000000,
-          marginLeft: "auto",
-          marginRight: "auto",
-          height: "100px",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        {/* <div
-          style={{
-            position: "fixed",
-            bottom: "0",
-            width: "400px",
-            zIndex: 1000000,
-            marginLeft: "auto",
-            marginRight: "auto",
-          }}
-        >
-          <AudioPlayer
-            src="/song.mp3"
-            autoPlay
-            volume={10}
-            color="white"
-            sliderColor="white"
-            style={{ backgroundColor: "transparent", color: "white" }}
-          />
-        </div> */}
-      </div>
 
       {loading ? (
         <div style={{ cursor: "none" }}>
@@ -188,133 +102,12 @@ export default function HomePage() {
             innerScale={0}
             outerScale={0}
             outerAlpha={0}
-            innerStyle={{
-              backgroundColor: "transparent",
-            }}
-            outerStyle={{
-              border: "0px solid transparent",
-            }}
+            innerStyle={{ backgroundColor: "transparent" }}
+            outerStyle={{ border: "0px solid transparent" }}
           />
           <Loader />
         </div>
       ) : (
-        <></>
-      )}
-      <div
-        style={{
-          position: "fixed",
-          top: displayGame ? 0 : -10000000000,
-          left: 0,
-          width: displayGame ? "100%" : "0%",
-          height: displayGame ? "100%" : "0%",
-          backgroundColor: "rgba(0, 0, 0, 0.8)",
-          zIndex: displayGame ? 999 : -1,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Iframe
-          url="https://pokemon-bryanc004.web.app"
-          styles={{
-            width: "80vw",
-            height: "80vh",
-            border: "none",
-          }}
-        />
-        <button
-          onClick={() => setDisplayGame(false)}
-          style={{
-            position: "absolute",
-            top: "20px",
-            right: "20px",
-            background: "black",
-            border: "none",
-            padding: "8px 16px",
-            borderRadius: "4px",
-            cursor: "pointer",
-            color: "white",
-          }}
-        >
-          Close Game
-        </button>
-      </div>
-
-      {showModal && !displayGame && (
-        <Modal
-          aria-labelledby="modal-title"
-          aria-describedby="modal-desc"
-          open={showModal}
-          onClose={() => setShowModal(false)}
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            caretColor: "black",
-          }}
-        >
-          <Sheet
-            variant="solid"
-            sx={{
-              maxWidth: 500,
-              borderRadius: "md",
-              p: 3,
-              boxShadow: "lg",
-              backgroundColor: "black",
-              color: "white",
-            }}
-          >
-            {" "}
-            <AnimatedCursor
-              innerSize={8}
-              outerSize={35}
-              innerScale={1}
-              outerScale={2}
-              outerAlpha={0}
-              innerStyle={{
-                backgroundColor: "red",
-              }}
-            />
-            <ModalClose variant="plain" sx={{ m: 1 }} />
-            <Typography
-              component="h2"
-              id="modal-title"
-              level="h4"
-              textColor="inherit"
-              sx={{ fontWeight: "lg", mb: 1 }}
-            >
-              Do you want to check out my Pokemon game?
-              <span style={{ color: "#ff8080", fontSize: "15px" }}>
-                (Strongly Recommended!)
-              </span>
-            </Typography>
-            <Typography id="modal-desc" textColor="white">
-              You get to control my dog (Pommy) with your keyboard in a Pokemon
-              setting and navigate through my portfolio. Please check it out!
-            </Typography>
-            <div style={{ width: "100%" }}>
-              <Button
-                sx={{
-                  marginTop: "10px",
-                  backgroundColor: "white",
-                  color: "black",
-                  marginLeft: "auto",
-                  display: "flex",
-                }}
-                onClick={() => {
-                  setDisplayGame(true);
-                  setShowModal(false);
-                  console.log("showing game");
-                }}
-              >
-                Yes!
-              </Button>
-            </div>
-          </Sheet>
-        </Modal>
-      )}
-
-      {!displayGame && (
         <>
           <AnimatedCursor
             innerSize={8}
@@ -322,12 +115,8 @@ export default function HomePage() {
             innerScale={1}
             outerScale={2}
             outerAlpha={0}
-            innerStyle={{
-              backgroundColor: "white",
-            }}
-            outerStyle={{
-              border: "3px solid white",
-            }}
+            innerStyle={{ backgroundColor: "white" }}
+            outerStyle={{ border: "3px solid white" }}
           />
           <div
             style={{
@@ -363,9 +152,7 @@ export default function HomePage() {
                       innerScale={1}
                       outerScale={2}
                       outerAlpha={0}
-                      innerStyle={{
-                        backgroundColor: "white",
-                      }}
+                      innerStyle={{ backgroundColor: "white" }}
                     />
                     <nav>
                       <ul
